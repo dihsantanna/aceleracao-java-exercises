74406856d7b9047b4bd9ff6789f98a87ea595594
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-03-30 12:57:59 -0300

    iniciando exercicio

--- src/main/java/com/trybe/acc/java/ConverteBytes.java
+++ src/main/java/com/trybe/acc/java/ConverteBytes.java
@@ -3,20 +3,27 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ConverteBytes {
-	
-	public static void main(String[] args) {
-			// Ler a entrada de um número pelo console
-			Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-			
-			System.out.println("Digite um número:");
-			byte valor = scan.nextByte();
-			
-			// Seu código aqui
-			
-			
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println(valor + " bytes correspondem a " + qtdBits + " bits");
+/**
+ * Class.
+ */
 
-		}
+public class ConverteBytes {
+  /**
+   * Method.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite um número:");
+    byte valor = scan.nextByte();
+    scan.close();
+
+    // Seu código aqui
+
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    // System.out.println(valor + " bytes correspondem a " + qtdBits + " bits");
+
+  }
 }
--- src/test/java/com/trybe/acc/java/ConverteBytesTest.java
+++ src/test/java/com/trybe/acc/java/ConverteBytesTest.java
@@ -9,33 +9,30 @@ import java.io.PrintStream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 class ConverteBytesTest {
-	
-	@Test
-	@DisplayName("1 - A aplicação pergunta, no terminal, um número")
-	public void testeMain() {
-		
-	     String userInput = String.format("3",
-	             System.lineSeparator()       
-	     );
-	     
-	     ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
-	     System.setIn(bais);
 
-	     String esperado = "3 bytes correspondem a 24 bits";
-	     
-	     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	     PrintStream printStream = new PrintStream(baos);
-	     System.setOut(printStream);
+  @Test
+  @DisplayName("1 - A aplicação pergunta, no terminal, um número")
+  public void testeMain() {
 
-	     ConverteBytes.main(null);
+    String userInput = String.format("3",
+        System.lineSeparator());
 
-	     String[] linha = baos.toString().split(System.lineSeparator());
-	     String saida = linha[linha.length-1];
-	     
-	     assertEquals(esperado,saida);
-    
-	     
-	}
+    ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+    System.setIn(bais);
+
+    String esperado = "3 bytes correspondem a 24 bits";
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(baos);
+    System.setOut(printStream);
+
+    ConverteBytes.main(null);
+
+    String[] linha = baos.toString().split(System.lineSeparator());
+    String saida = linha[linha.length - 1];
+
+    assertEquals(esperado, saida);
+
+  }
 }
