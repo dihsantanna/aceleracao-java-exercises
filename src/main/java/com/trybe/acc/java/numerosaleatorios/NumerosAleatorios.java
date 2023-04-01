a66f7951ee0f7b4021a1659f117407a1938a7717
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-04-01 01:25:06 -0300

    termina implementação da classe NumerosAleatorios

--- src/main/java/com/trybe/acc/java/numerosaleatorios/NumerosAleatorios.java
+++ src/main/java/com/trybe/acc/java/numerosaleatorios/NumerosAleatorios.java
@@ -2,21 +2,27 @@ package com.trybe.acc.java.numerosaleatorios;
 
 import java.util.Random;
 
+/**
+ * Classe NumerosAleatorios.
+ */
+
 public class NumerosAleatorios {
   /**
-   * Método principal para a execuçao do código.
-   * 
+   * Método principal para a execuçao do código. 
    */
   public static void main(String[] args) {
-    // Seu código vai aqui
+    int numero = numeroAleatorio();
+    while(numero != 4 ) {
+      imprime(numero);
+      numero = numeroAleatorio();
+    }
+    imprime(numero);
   }
 
-  // Chame a função escrevendo `imprime()`.
   static void imprime(int numero) {
     System.out.println(numero);
   }
 
-  // Chame a função escrevendo `numeroAleatorio()`.
   static int numeroAleatorio() {
     return numerosAleatorios.entreUmECinco();
   }
