76ec416496acb611f2a4b1509ec55170ce9e5097
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-04-01 02:43:03 -0300

    termina implementação das classe Pessoa e Main

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -2,12 +2,43 @@ package com.trybe.idadeemdias;
 
 import java.util.Scanner;
 
+/**
+ * Classe Main.
+ */
+
 public class Main {
+  /**
+   * Metodo main.
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
+    System.out.println("Qual é o seu nome?");
 
-		
-	}
+    String nome = scan.next();
+
+    System.out.println("Qual é sua idade em anos, meses e dias?");
+    System.out.println("anos:");
+
+    String anos = scan.next();
+
+    System.out.println("meses:");
+
+    String meses = scan.next();
+
+    System.out.println("dias:");
+
+    String dias = scan.next();
+
+    scan.close();
+
+    Pessoa pessoa = new Pessoa();
+    int idadeEmDias = pessoa.calcularIdadeEmDias(
+        Integer.parseInt(anos),
+        Integer.parseInt(meses),
+        Integer.parseInt(dias));
+
+    System.out.println("Olá " + nome + ", sua idade em dias é " + idadeEmDias);
+  }
 
 }
--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -1,7 +1,19 @@
 package com.trybe.idadeemdias;
 
-public class Pessoa {
-	
+/**
+ * Classe Pessoa.
+ */
 
-	
+public class Pessoa {
+  /**
+   * Metodo calcularIdadeEmDias. Para simplificar, foi considerado que todos os
+   * anos possuem 365 dias e todos os meses 30.
+   */
+  public int calcularIdadeEmDias(int anos, int meses, int dias) {
+    int anosEmDias = anos * 365;
+    int mesesEmDias = meses * 30;
+
+    return anosEmDias + mesesEmDias + dias;
+  }
+
 }
